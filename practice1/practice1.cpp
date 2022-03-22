e7cf021856e44f2e99d36ef2b5279fb9b95754b3
ohne <ghdlr785@gmail.com>
2022-03-22 15:53:09 +0900

    수정

--- practice1/practice1.cpp
+++ practice1/practice1.cpp
@@ -2,9 +2,9 @@
 
 enum Command
 {
-    Push = 1,
-    Pop = 2,
-    Exit = 3
+    PUSH = 1,
+    POP = 2,
+    EXIT = 3
 };
 
 const int STACK_SIZE{ 10 };
@@ -12,7 +12,7 @@ const int STACK_SIZE{ 10 };
 struct Stack
 {
     int array[STACK_SIZE]{};
-    int topIndex{-1};
+    int topIndex{ -1 };
 };
 
 void PrintInfo()
@@ -21,22 +21,29 @@ void PrintInfo()
     std::cout << "[1] push" << std::endl;
     std::cout << "[2] pop" << std::endl;
     std::cout << "[3] exit" << std::endl;
-    std::cout << "------------" << std::endl;
+    std::cout << "-------------" << std::endl;
 }
 
-void printStack(Stack& stack)
+void PrintStack(Stack& stack)
 {
     std::cout << "<STACK>" << std::endl;
     if (stack.topIndex < 0)
     {
         std::cout << "EMPTY" << std::endl;
     }
+
+    for (int i = stack.topIndex; i >= 0; --i)
+    {
+        std::cout << stack.array[i] << std::endl;
+    }
+    std::cout << "-----------------";
 }
+
 void PushStack(Stack& stack, int value)
 {
     if (stack.topIndex >= STACK_SIZE - 1)
     {
-        std::cout << "스택이 가득 찼습니다!! 더 이상 추가할 수 없어요!" << std::endl;
+        std::cout << "스택이 가득 찼습니다! 더 이상 추가할 수 없어요!" << std::endl;
         return;
     }
     stack.array[++stack.topIndex] = value;
@@ -46,10 +53,12 @@ void PopStack(Stack& stack)
 {
     if (stack.topIndex < 0)
     {
-        std::cout << "스택이 비어 있습니다!! 더 이상 꺼낼 수 없어요!" << std::endl;
+        std::cout << "스택이 비어 있습니다! 더 이상 꺼낼 수 없어요!" << std::endl;
         return;
     }
-    std::cout << stack.array[stack.topIndex--] << "를 꺼냇습니다." << std::endl;
+
+    std::cout << stack.array[stack.topIndex--] << "를 꺼냈습니다." << std::endl;
+
 }
 
 void ProcessUserInput(Stack& stack)
@@ -57,37 +66,41 @@ void ProcessUserInput(Stack& stack)
     int command{};
     bool isExit{ false };
 
-    while (true)
+    while (command != EXIT)
     {
-        printStack(stack);
+        PrintStack(stack);
         std::cout << std::endl << "> ";
         std::cin >> command;
         switch (command)
         {
-            case Push:
-                int value;
-                std::cout << "    value>>" ;
-                std::cin >> value;
-                    PushStack(stack, value);
+        case PUSH:
+        {
+            int value;
+            std::cout << "    Value>>";
+            std::cin >> value;
+            PushStack(stack, value);
             break;
-            case Pop:
-                PopStack(stack);
-                break;
-            case Exit:
-                isExit = true;
-                break;
-            default:
-                std::cout << "잘못된 명령어 입니다 ." << std::endl;
-                break;
         }
+
+        case POP:
+            PopStack(stack);
+            break;
+
+        case EXIT:
+            isExit = true;
+            break;
+
+        default:
+            std::cout << "잘못된 명령어 입니다." << std::endl;
+            break;
+        }
+
         if (isExit)
         {
-
+            break;
         }
     }
 }
-
-
 
 int main()
 {
